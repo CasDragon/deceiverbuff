4f287176c0f0042a7bb83cfacbe36c6373cbabfe
CasDragon <redyugi112@gmail.com>
2024-06-16 10:25:41 -0400

    Add Hellfire Ray to spell list

--- deceiverbuff/Content/SpellList.cs
+++ deceiverbuff/Content/SpellList.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using BlueprintCore.Blueprints.CustomConfigurators.Classes.Spells;
+using BlueprintCore.Blueprints.CustomConfigurators.UnitLogic.Abilities;
+using BlueprintCore.Blueprints.References;
+using Kingmaker.Blueprints.Classes.Spells;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +14,18 @@ namespace deceiverbuff.Content
     {
         public static void Configure()
         {
+            Main.logger.Info("Starting Spelllist Configure");
             // todo
+            // Hellfire Ray, Magic Missle, cantrips, Energy Drain, Ill Omen, Polar Ray, Wave of Exhaustion, Wave of Fatigue, Tidal Wave, Black Tentacles, Infernal Healing
+            AddHellFire();
+            Main.logger.Info("Completed Spelllist Configure");
+        }
+        public static void AddHellFire()
+        {
+            Main.logger.Verbose("Adding Hellfire Ray To Deceiver");
+            AbilityConfigurator.For(AbilityRefs.HellfireRay.Reference.Get())
+                .AddToSpellList(level: 6, SpellbookRefs.MagicDeceiverSpellbook.Reference.Get())
+                .Configure();
         }
     }
 }
